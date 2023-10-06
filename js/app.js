b464367abbc99366857edf9059499bca7af510c2
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-06 17:18:00 +0200

    ADD - index, readme and app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// ## FIZZBUZZ
+
+// - Dichiarare il ciclo dei numeri
+//     - SE sono multipli di 3
+//       - stampare "Fizz"
+//     - ALTRIMENTI SE sono multipli di 5
+//       - stampare"Buzz"
+//     - ALTRIMENTI SE sono multipli di 3 e 5
+//       - stampare "FizzBuzz"
