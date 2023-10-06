9025fea56c971ed382a7c19063825e8a24f30293
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-06 17:27:02 +0200

    ADD - ciclo per i numeri da 1 a 100

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 // ## FIZZBUZZ
 
 // - Dichiarare il ciclo dei numeri
+
+for (let i = 0; i < 100; i++){
+    n = i + 1
+    console.log (n)
+}
+
 //     - SE sono multipli di 3
 //       - stampare "Fizz"
 //     - ALTRIMENTI SE sono multipli di 5
