bdbc134e13f76d9fb26daf2e60354662061bd513
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-09 15:25:08 +0200

    ADD - esercizio finito

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## FIZZBUZZ
 
 - Dichiarare il ciclo dei numeri
-    - SE sono multipli di 3
-      - stampare "Fizz"
+    - SE sono multipli di 3 e 5
+       - stampare "FizzBuzz"
+    - ALTRIMENTI SE sono multipli di 3
+      - stampare"fizz"
     - ALTRIMENTI SE sono multipli di 5
-      - stampare"Buzz"
-    - ALTRIMENTI SE sono multipli di 3 e 5
-      - stampare "FizzBuzz"
+      - stampare "Buzz"
--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 // ## FIZZBUZZ
 
+
 // - Dichiarare il ciclo dei numeri
 
-for (let i = 0; i < 100; i++){
+ for (let i = 0; i < 100; i++){
     n = i + 1
-    console.log (n)
+    m3 = n % 3
+    m5 = n % 5
+
+
+    // - SE sono multipli di 3 e 5
+    //   - stampare "FizzBuzz"
+    if (m3 === 0 && m5 === 0){
+        console.log ("FizzBuzz")
+    } 
+    // ALTRIMENTI SE sono multipli di 3
+    //   - stampare "Fizz"
+    else if (m3 === 0){
+        console.log ("Fizz")
+    }
+
+    // - ALTRIMENTI SE sono multipli di 5
+    //   - stampare"Buzz"
+    else if (m5 === 0){
+        console.log("Buzz")
+    }
+    else {
+        console.log(n)
+    }
+    
 }
 
-//     - SE sono multipli di 3
-//       - stampare "Fizz"
-//     - ALTRIMENTI SE sono multipli di 5
-//       - stampare"Buzz"
-//     - ALTRIMENTI SE sono multipli di 3 e 5
-//       - stampare "FizzBuzz"
+
